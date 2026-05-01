0b506672466de496936ed059988f04124208a576
WellingtonShowcase <wellingtonvasconcelos223@gmail.com>
2026-05-01 12:43:38 -0300

    01/05/2026 - ROSA scroll

---
+++ p.ROSA/p.RosaE.js
@@ -0,0 +1,47 @@
+const sens = 100;
+let Iy = 0;
+
+function scroll(d) {
+    const scrollT = window.innerHeight;
+    window.scrollBy({
+        top: d === 'down' ? scrollT : -scrollT, behavior: 'smooth'
+    });
+}
+
+
+window.addEventListener('wheel', (e) => {
+    if (e.deltaY > 0) {
+        scroll('down');
+    } else {
+        scroll('up');
+    }
+}, { passive: true});
+
+
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowDown' || e.key === '') {
+        e.preventDefault();
+        scroll('down');
+    } else if (e.key === 'ArrowUp') {
+        e.preventDefault();
+        scroll('up');
+    }
+});
+
+window.addEventListener('touchstart', (e) => {
+    mobileIy = e.touches[0].clientY;
+}, { passive: true });
+
+window.addEventListener('touchend', (e) => {
+    const mobileFy = e.changedTouches[0].clientY;
+    const dif = mobileIy - mobileFy;
+
+    if (Math.abs(dif) > sens) {
+        if (dif > 0) {
+            scroll('down');
+        } else {
+            scroll('up');
+        }
+    }
+}, { passive: true });
